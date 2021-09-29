4bb0fac5f5589a416aa9f32beab765bb99efb15f
Diamond Morehead <90802640+DiamondMorehead@users.noreply.github.com>
2021-09-29 12:44:10 -0700

    prep-javascript-functions

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,14 @@
+function addTwoNumbers(x, y) {
+  return x + y;
+}
+
+var addTwoNumbersResult = addTwoNumbers(2, 2);
+console.log('addTwoNumbers Exercise:', addTwoNumbersResult);
+
+function convertHoursToMinutes(h) {
+  return h * 60;
+}
+
+var convertHoursToMinutesResult = convertHoursToMinutes(2);
+console.log('convertHoursToMinutes Exercise:', convertHoursToMinutesResult)
+;
