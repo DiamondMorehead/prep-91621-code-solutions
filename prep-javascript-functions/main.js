f9c25b6bbd0e5471fada30cf56e8d1a89dcc3428
Diamond Morehead <90802640+DiamondMorehead@users.noreply.github.com>
2021-09-29 15:56:22 -0700

    prep-javascript-functions

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -12,3 +12,10 @@ function convertHoursToMinutes(h) {
 var convertHoursToMinutesResult = convertHoursToMinutes(2);
 console.log('convertHoursToMinutes Exercise:', convertHoursToMinutesResult)
 ;
+function getGreeting(name) {
+  return 'Hello' + ' ' + name + '!'
+  ;
+}
+var getGreetingResult = getGreeting('Diamond');
+console.log(getGreetingResult);
+// "Hello, Diamond!"
