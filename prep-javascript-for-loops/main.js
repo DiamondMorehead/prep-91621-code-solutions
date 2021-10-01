8361335716e779f56576c8869dbc90dfa30d56e8
Diamond Morehead <90802640+DiamondMorehead@users.noreply.github.com>
2021-09-30 21:10:49 -0700

    Completed prep-javascript-for-loops exercise.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,10 @@
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+for (var x = 0; x < 20; x += 2) {
+  console.log(x);
+}
+
+for (var n = 100; n > 0; n--) {
+  console.log('Time till explosion ' + n + '!');
+}
